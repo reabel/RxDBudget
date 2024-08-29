68d023a348df30a8927d5acf04f1d2fc05293d35
reabel <ryanreabel@gmail.com>
2024-08-28 22:24:32 -0400

    tying store to form

--- frontend/budget-vue/src/components/BudgetForm.vue
+++ frontend/budget-vue/src/components/BudgetForm.vue
@@ -1,37 +1,48 @@
 <script setup lang="ts">
-
+import { useBudgetStore } from '../stores/budgetstore';
+import { ref } from 'vue';
+const store = useBudgetStore();
+const category = ref('');
+const submit = () => {
+  console.log('submitting');
+  store.addLineItem({
+    name: 'test',
+    type: category.value,
+    price: 100,
+    recurring: false,
+    date: new Date(),
+  });
+};
 </script>
 
 <template>
   <h1>Budget Form</h1>
+  <div style="display: flex">
+    <div>
+      <label for="category">Category</label>
+      <input type="select" id="category" :value="category" name="category" />
+    </div>
 
-  <div>
-    <label for="category">Category</label>
-    <input type="select" id="category" name="category">
+    <div>
+      <label for="amount">Amount</label>
+      <input type="currency" id="amount" name="amount" />
+    </div>
+
+    <div>
+      <label for="recurring">Recurring?</label>
+      <input type="checkbox" id="recurring" name="recurring" />
+    </div>
+
+    <div>
+      <label for="date">Date</label>
+      <input type="date" id="date" name="date" />
+    </div>
   </div>
-
-  <div>
-    <label for="amount">Amount</label>
-    <input type="currency" id="amount" name="amount">
-  </div>
-
-  
-  <div>
-    <label for="recurring">Recurring?</label>
-    <input type="checkbox" id="recurring" name="recurring">
-  </div>
-
-  <div>
-    <label for="date">Date</label>
-    <input type="date" id="date" name="date">
-  </div>
-
-
+  <button @click="submit">Submit</button>
 </template>
 
 <style scoped>
 .read-the-docs {
-    color: #888;
+  color: #888;
 }
-
 </style>
