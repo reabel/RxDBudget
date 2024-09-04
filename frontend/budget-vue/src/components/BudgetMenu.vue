236e04bd8bba47db03ed339c313f38e8f1455c1d
Ryan Reabel <ryanreabel@gmail.com>
2024-09-04 01:05:25 -0400

    styling, adding menu as it's own component

---
+++ frontend/budget-vue/src/components/BudgetMenu.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useBudgetStore } from '../stores/budgetstore';
+import { storeToRefs } from 'pinia';
+const store = useBudgetStore();
+const { displayForm } = storeToRefs(store);
+</script>
+<template>
+    <menu>
+      <button v-if="!displayForm" @click="store.toggleDisplayForm();">Add Line ...</button>
+      <button v-else @click="store.toggleDisplayForm()">Close Form</button>
+      <button>Import ...</button>
+      <button>Export ...</button>
+   </menu>
+</template>
+
+<style scoped>
+/* Top menu, align with top-right of screen, no background */
+menu {
+  position: fixed;
+  top: 0;
+  right: 0;
+  background: none;
+  padding: 0.5em;
+  display: flex;
+  flex-direction: row;
+  gap: 0.5em;
+}
+</style>
