5ff9d74841c462399c88c5420631237fcccbeb0a
reabel <ryanreabel@gmail.com>
2024-09-11 01:28:04 -0400

    Adding capability to download store values (currently as JSON, todo as csv).

--- frontend/budget-vue/src/components/BudgetMenu.vue
+++ frontend/budget-vue/src/components/BudgetMenu.vue
@@ -2,14 +2,36 @@
 import { useBudgetStore } from '../stores/budgetstore';
 import { storeToRefs } from 'pinia';
 const store = useBudgetStore();
-const { displayForm } = storeToRefs(store);
+const { displayForm, lineItems } = storeToRefs(store);
+const downloadItem = (item?: any) => {
+  console.log('Download item', item);
+
+    // const blob = new Blob([lineItems.value],  { type: 'application/pdf' })
+        // const link = document.createElement('a')
+        // link.href = URL.createObjectURL(blob)
+        // link.download = label
+        // link.click()
+        // URL.revokeObjectURL(link.href)
+
+  let text = JSON.stringify(lineItems);
+  let filename = 'budgets.json';
+  let element = document.createElement('a');
+  element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+  document.body.removeChild(element);  
+}
 </script>
 <template>
     <menu>
       <button v-if="!displayForm" @click="store.toggleDisplayForm();">Add Line ...</button>
       <button v-else @click="store.toggleDisplayForm()">Close Form</button>
       <button>Import ...</button>
-      <button>Export ...</button>
+      <button @click.prevent="downloadItem()">Export ...</button>
    </menu>
 </template>
 
